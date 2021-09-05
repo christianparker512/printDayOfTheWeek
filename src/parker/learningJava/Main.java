f3401766a6ae2734282e9ee7495090340bf4298e
Christian Parker <christianparker512@gmail.com>
2021-09-05 18:49:56 -0500

    if else solution

--- src/parker/learningJava/Main.java
+++ src/parker/learningJava/Main.java
@@ -16,35 +16,53 @@ public class Main {
 
     }
     private static void printDayOfTheWeek(int day){
-        switch(day){
-            case 0:
-                System.out.println("Sunday");
-                break;
-            case 1:
-                System.out.println("Monday");
-                break;
-            case 2:
-                System.out.println("Tuesday");
-                break;
-            case 3:
-                System.out.println("Wednesday");
-                break;
-            case 4:
-                System.out.println("Thursday");
-                break;
-            case 5:
-                System.out.println("Friday");
-                break;
-            case 6:
-                System.out.println("Saturday");
-                break;
-            case 7:
-                System.out.println("Sunday");
-                break;
+        if (day== 0) {
+            System.out.println("sunday");
+        }else if(day==1){
+            System.out.println("monday");
+        }else if(day==2){
+            System.out.println("tuesday");
+        }else if(day==3){
+            System.out.println("wednesday");
+        }else if(day==4){
+            System.out.println("thursday");
+        }else if(day==5){
+            System.out.println("friday");
+        }else if(day==6) {
+            System.out.println("saturday");
+        } else {
+            System.out.println("Invalid day");
+        }
 
-            default:
-                System.out.println("Invalid day");
-                break;
+//        switch(day){
+//            case 0:
+//                System.out.println("Sunday");
+//                break;
+//            case 1:
+//                System.out.println("Monday");
+//                break;
+//            case 2:
+//                System.out.println("Tuesday");
+//                break;
+//            case 3:
+//                System.out.println("Wednesday");
+//                break;
+//            case 4:
+//                System.out.println("Thursday");
+//                break;
+//            case 5:
+//                System.out.println("Friday");
+//                break;
+//            case 6:
+//                System.out.println("Saturday");
+//                break;
+//            case 7:
+//                System.out.println("Sunday");
+//                break;
+//
+//            default:
+//                System.out.println("Invalid day");
+//                break;
         }
     }
-}
+//}
